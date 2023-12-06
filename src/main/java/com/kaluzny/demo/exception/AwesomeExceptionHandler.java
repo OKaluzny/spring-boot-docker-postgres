e2db14d96e18f00931b27a09a28307a8697980c9
OKaluzny <kaluzny.oleg@gmail.com>
2023-12-06 11:46:03 +0200

    Added some check for exceptions

--- src/main/java/com/kaluzny/demo/exception/AwesomeExceptionHandler.java
+++ src/main/java/com/kaluzny/demo/exception/AwesomeExceptionHandler.java
@@ -1,29 +1,51 @@
 package com.kaluzny.demo.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+import java.util.concurrent.TimeoutException;
+
+@RestControllerAdvice
 public class AwesomeExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(ThereIsNoSuchAutoException.class)
-    public ResponseEntity<AwesomeException> handleThereIsNoSuchUserException() {
-        return new ResponseEntity<>(new AwesomeException("There is no such automobile"), HttpStatus.NOT_FOUND);
+    @ExceptionHandler(RuntimeException.class)
+    public ProblemDetail handleRuntimeException(RuntimeException ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(404), ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    public ProblemDetail handleBadRequest(HttpClientErrorException.BadRequest ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), ex.getMessage());
     }
 
     @ExceptionHandler(AutoWasDeletedException.class)
-    public ResponseEntity<AwesomeException> handleDeleteException() {
-        return new ResponseEntity<>(new AwesomeException("This auto was deleted"), HttpStatus.NOT_FOUND);
+    public ProblemDetail handleDeleteException(AutoWasDeletedException ex) {
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(404), ex.getMessage());
+        pd.setTitle("This auto was deleted");
+        return pd;
     }
 
-    private static class AwesomeException {
-        String message;
+    @ExceptionHandler(ThereIsNoSuchAutoException.class)
+    public ProblemDetail handleThereIsNoSuchUserException(ThereIsNoSuchAutoException ex) {
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(404), ex.getMessage());
+        pd.setTitle("There is no such automobile");
+        return pd;
+    }
 
-        public AwesomeException(String message) {
-            this.message = message;
-        }
+    @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
+    public ProblemDetail handleConnectException(HttpServerErrorException.InternalServerError ex) {
+        return ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), ex.getMessage());
+    }
+
+    @ExceptionHandler(TimeoutException.class)
+    public ProblemDetail handleTimeoutException(TimeoutException ex) {
+        ProblemDetail pd = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(503), ex.getMessage());
+        pd.setTitle("Service Unavailable or DB connection was refused");
+        return pd;
     }
 }
--- src/main/java/com/kaluzny/demo/exception/ThereIsNoSuchAutoException.java
+++ src/main/java/com/kaluzny/demo/exception/ThereIsNoSuchAutoException.java
@@ -1,8 +1,4 @@
 package com.kaluzny.demo.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-//@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "There is no such automobile")
-public class ThereIsNoSuchAutoException extends RuntimeException{
+public class ThereIsNoSuchAutoException extends RuntimeException {
 }
